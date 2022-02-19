bf55edf362857e00d5543e6e300cd8f5469d7271
Serraznet Gaming <c_serrao@hotmail.com>
2022-02-19 14:10:31 +1100

    Removed menu options
    setup for qtarget and item use for spikestrips

--- config.lua
+++ config.lua
@@ -4,7 +4,7 @@ Config = {
     -- video: https://gyazo.com/8f713a51b2fd2a1e2faa7735e2cc1921
     NPCVehicles = false, -- should the script burst tyres of npcs vehicles? (NOTE: PERFORMANCE HEAVY FOR THE CLIENTS!  ~0.03 - 0.20 ms)
     
-    Framework = "none", --[[ What framework to use
+    Framework = "esx", --[[ What framework to use
         Valid options:
             * "qb" (qb-core)
             * "esx" (ESX)
@@ -12,20 +12,13 @@ Config = {
     ]]
 
     RequireJobPlace = true, -- require job to place a spike strip?
-    RequireJobRemove = false, -- should everyone be able to remove a spikestrip, or just people with job allowed to place spikestrips?
-    
-    Menu = {
-        Enabled = true, -- TriggerEvent("loaf_spikestrips:spikestripMenu") to open the menu
-        Command = "spikestrip", -- set to false to disable
-        Keybind = "F5", -- set to false to disable (NOTE: COMMAND CAN'T BE FALSE IF YOU WANT A KEYBIND)
-    },
+    RequireJobRemove = true, -- should everyone be able to remove a spikestrip, or just people with job allowed to place spikestrips?
     
     FrameworkFeatures = { -- these features are only if you use Config.Framework "esx" or "qb"
         Item = "spikestrip", -- item to deploy a spikestrip (set to false if you don't want to have this enabled)
         ReceiveRemove = true, -- receive spikestrip item if you remove a spikestrip?
         ReceiveJob = true, -- false = police won't receive a spikestrip when they remove it | true = police will receive a spikestrip item when they remove a spikestrip
         
-        UseWarmenu = false, -- false = default esx menu, true = use warmenu (looks like gta:o)
         PoliceJobs = { -- police jobs
             "police",
             "sheriff",
